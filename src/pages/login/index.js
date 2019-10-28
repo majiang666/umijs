2e0e82b08abb35480796dbc13f8055a1379517f9
cssking <laji198888@163.com>
2019-10-28 23:03:19 +0800

    umijs 使用hook

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,11 @@
+import React,{ useState } from 'react';
 export default () => {
+    const [count,setCount] = useState(0);
     return(
-        <h1>Login</h1>
+        <h1>
+            Login
+            {count} 
+            <button onClick={() => setCount(count+1)}>+1</button>
+        </h1>
     )
 }
